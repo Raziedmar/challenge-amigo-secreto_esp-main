08d165071a548d8086c630ddc669bc2c685a8c97
Raziedmar <edmario84@gmail.com>
2025-08-27 10:45:56 -0500

    Crea la función de Agregar Amigo y Actualiza la función de validacion de la entrada

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let entrada = "";
+let listaDeAmigos = [];
+let entradaValida = false;
 
 function agregarAmigo(){
 
     entrada = document.getElementById('amigo').value;
-    validarEntrada(entrada);
+    if(validarEntrada(entrada)){
+        listaDeAmigos.push(entrada);
+        console.log(listaDeAmigos);
+    }else {
+        alert("Ingresa nombre válido");
+    }    
 }
+
 function validarEntrada(entrada){
     
     if(entrada ==""){
-        alert("Ingresa nombre válido");
+        return false;
     }else {
-        console.log(entrada);
-        return entrada;
+        return true;
     }
 }
