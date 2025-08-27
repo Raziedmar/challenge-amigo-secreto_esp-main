69c3328d52c910db11e289dacb8a1ca4125c0fc7
Raziedmar <edmario84@gmail.com>
2025-08-27 10:27:42 -0500

    Crea la funcion de validación de la entrada del nombre del amigo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let entrada = "";
+
+function agregarAmigo(){
+
+    entrada = document.getElementById('amigo').value;
+    validarEntrada(entrada);
+}
+function validarEntrada(entrada){
+    
+    if(entrada ==""){
+        alert("Ingresa nombre válido");
+    }else {
+        console.log(entrada);
+        return entrada;
+    }
+}
